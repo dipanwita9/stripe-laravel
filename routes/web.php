8c3d470af9ed44d3a61c05d48db4f1c7e2811fbd
dipanwita d <dips@localhost.localdomain>
2023-06-02 16:07:33 +0530

    Stripe payment with laravel

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StripeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [StripeController::class,'index']);
+Route::post('/checkout',[StripeController::class,'checkout']);
+Route::get('/success',[StripeController::class,'success']);
